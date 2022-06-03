1008a29bd26ead846f17c2c9b46cf9650f1c9caa
Ivan Azis <haloivan.idn@gmail.com>
2022-06-03 23:48:43 +0700

    add favicon and sites meta

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <!-- sites meta -->
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Init Project</title>
+  <meta name="description" content="This website is the final project of the creator for the lecture on creating a digital open library website of UIN Maulana Malik Ibrahim Malang">
+  <meta name="keywords" content="Final Project, Open Library, Digital Library">
+  <meta name="author" content="Meidhita Nurwegia Putri">
+
+  <title>Malang Open Public Library</title>
+  
+  <link rel="shortcut icon" href="assets/images/icons/Books/favicon.ico" type="image/x-icon">
 </head>
 <body>
   <div>
