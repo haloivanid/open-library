df483ea20a779e6b9e5210b671471acd2f5a57ae
Ivan Azis <haloivan.idn@gmail.com>
2022-06-06 21:12:54 +0700

    change db creds

--- databases/connection.php
+++ databases/connection.php
@@ -1,5 +1,5 @@
 <?php 
-  $koneksi = mysqli_connect("localhost","haloivan","Van@010168","library");
+  $koneksi = mysqli_connect("8.219.122.52","root","Van@010168","OpenLibraryDB");
   if (mysqli_connect_error()){
     echo "Koneksi database gagal : " . mysqli_connect_error();
   }
